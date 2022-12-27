f68004701e3000e47bf35530d9c8b322b6494b95
Aliaksei Shkurko <shaleprog@gmail.com>
2022-12-27 17:40:36 +0300

    add annotation to data provider and fix incorrect test's

--- src/test/java/by/x1ss/arrayTask/StreamsMethodTest.java
+++ src/test/java/by/x1ss/arrayTask/StreamsMethodTest.java
@@ -1,5 +1,6 @@
 package by.x1ss.arrayTask;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Assert;
@@ -8,11 +9,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(DataProviderRunner.class)
 public class StreamsMethodTest {
-    public Object[][] data() {
+    @DataProvider
+    public static Object[][] data() {
         return new Object[][]{
                 {null, new int[]{0, 0}},
                 {new int[0], new int[]{0, 0}},
-                {new int[]{1, 2, 3, 4, 5}, new int[]{5, -15}},
+                {new int[]{1, 2, 3, 4, 5}, new int[]{5, 0}},
                 {new int[]{-1, -2, -3, -4, -5}, new int[]{0, -15}},
                 {new int[]{1, 2, 3, 4, -5}, new int[]{4, -5}},
                 {new int[]{}, new int[]{0, 0}},
@@ -27,7 +29,7 @@ public class StreamsMethodTest {
                 {new int[]{-1, 0, 0}, new int[]{0, -1}},
                 {new int[]{0, -1, 0}, new int[]{0, -1}},
                 {new int[]{0, 0, -1}, new int[]{0, -1}},
-                {new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, new int[]{10, -55}},
+                {new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, new int[]{10, 0}},
                 {new int[]{-1, -2, -3, -4, -5, -6, -7, -8, -9, -10}, new int[]{0, -55}},
                 {new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, -10}, new int[]{9, -10}},
                 {new int[]{1, 2, 3, 4, 5, 6, 7, 8, -9, -10}, new int[]{8, -19}},
