c2bd1e06a7ed125ed0ba687c1fb5a808c1575a67
Aliaksei Shkurko <shaleprog@gmail.com>
2022-12-27 19:23:03 +0300

    edit tests

--- src/test/java/by/x1ss/arrayTask/StreamsMethodTest.java
+++ src/test/java/by/x1ss/arrayTask/StreamsMethodTest.java
@@ -16,23 +16,7 @@ public class StreamsMethodTest {
                 {new int[0], new int[0]},
                 {new int[]{1, 2, 3, 4, 5}, new int[]{5, 0}},
                 {new int[]{-1, -2, -3, -4, -5}, new int[]{0, -15}},
-                {new int[]{1, 2, 3, 4, -5}, new int[]{4, -5}},
-                {new int[]{}, new int[]{0, 0}},
-                {new int[]{0}, new int[]{0, 0}},
-                {new int[]{1}, new int[]{1, 0}},
-                {new int[]{-1}, new int[]{0, -1}},
-                {new int[]{1, 0, -1}, new int[]{1, -1}},
-                {new int[]{0, 0, 0}, new int[]{0, 0}},
-                {new int[]{1, 0, 0}, new int[]{1, 0}},
-                {new int[]{0, 1, 0}, new int[]{1, 0}},
-                {new int[]{0, 0, 1}, new int[]{1, 0}},
-                {new int[]{-1, 0, 0}, new int[]{0, -1}},
-                {new int[]{0, -1, 0}, new int[]{0, -1}},
-                {new int[]{0, 0, -1}, new int[]{0, -1}},
-                {new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, new int[]{10, 0}},
-                {new int[]{-1, -2, -3, -4, -5, -6, -7, -8, -9, -10}, new int[]{0, -55}},
-                {new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, -10}, new int[]{9, -10}},
-                {new int[]{1, 2, 3, 4, 5, 6, 7, 8, -9, -10}, new int[]{8, -19}},
+                {new int[]{1, 2, 3, 4, -5}, new int[]{4, -5}}
         };
     }
 
